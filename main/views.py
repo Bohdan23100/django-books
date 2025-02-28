143bd516204b271de228f2ecd8bb3b1ab8ae5182
Bohdan23100 <23102008bv@gmail.com>
2025-02-28 22:37:30 +0100

    commit3

--- main/views.py
+++ main/views.py
@@ -6,6 +6,10 @@ from django.shortcuts import render
 def main(request):
     return render(request,'main/main.html')
 
+def profile(request):
+    return render(request, 'main/profile.html')
+
+
 
 
 
